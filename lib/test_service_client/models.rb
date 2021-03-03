85a916f208c89f54ce607fe623eef69b241f625f
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2021-03-02 23:14:26 -0500

    Fresh specgen plugin

--- lib/test_service_client/models.rb
+++ lib/test_service_client/models.rb
@@ -5,152 +5,150 @@ require "test_service_client/enum"
 require "test_service_client/dataclass"
 
 module TestService
-  module Client
-    class Message
-      include DataClass
-      val :int_field, Integer
-      val :string_field, String
-      def initialize(int_field:, string_field:)
-        super method(__method__).parameters.map { |parts| [parts[1], eval(parts[1].to_s)] }.to_h
-      end
-      
+  class Message
+    include DataClass
+    val :int_field, Integer
+    val :string_field, String
+    def initialize(int_field:, string_field:)
+      super method(__method__).parameters.map { |parts| [parts[1], eval(parts[1].to_s)] }.to_h
     end
     
-    class Nested
-      include DataClass
-      val :field, String
-      def initialize(field:)
-        super method(__method__).parameters.map { |parts| [parts[1], eval(parts[1].to_s)] }.to_h
-      end
-      
-    end
-    
-    class Parent
-      include DataClass
-      val :field, String
-      val :nested, Nested
-      def initialize(field:, nested:)
-        super method(__method__).parameters.map { |parts| [parts[1], eval(parts[1].to_s)] }.to_h
-      end
-      
-    end
-    
-    class Choice
-      include Enum
-      define :first_choice, 'FIRST_CHOICE'
-      define :second_choice, 'SECOND_CHOICE'
-      define :third_choice, 'THIRD_CHOICE'
-    end
-    
-    class EnumFields
-      include DataClass
-      val :enum_field, Choice
-      def initialize(enum_field:)
-        super method(__method__).parameters.map { |parts| [parts[1], eval(parts[1].to_s)] }.to_h
-      end
-      
-    end
-    
-    class NumericFields
-      include DataClass
-      val :byte_field, Integer
-      val :short_field, Integer
-      val :int_field, Integer
-      val :long_field, Integer
-      val :float_field, Float
-      val :double_field, Float
-      val :decimal_field, Float
-      def initialize(byte_field:, short_field:, int_field:, long_field:, float_field:, double_field:, decimal_field:)
-        super method(__method__).parameters.map { |parts| [parts[1], eval(parts[1].to_s)] }.to_h
-      end
-      
-    end
-    
-    class NonNumericFields
-      include DataClass
-      val :boolean_field, Boolean
-      val :char_field, String
-      val :string_field, String
-      val :uuid_field, UUID
-      val :date_field, Date
-      val :datetime_field, DateTime
-      val :time_field, TimeOfDay
-      def initialize(boolean_field:, char_field:, string_field:, uuid_field:, date_field:, datetime_field:, time_field:)
-        super method(__method__).parameters.map { |parts| [parts[1], eval(parts[1].to_s)] }.to_h
-      end
-      
-    end
-    
-    class ArrayFields
-      include DataClass
-      val :int_array_field, T.array(Integer)
-      val :string_array_field, T.array(String)
-      def initialize(int_array_field:, string_array_field:)
-        super method(__method__).parameters.map { |parts| [parts[1], eval(parts[1].to_s)] }.to_h
-      end
-      
-    end
-    
-    class MapFields
-      include DataClass
-      val :int_map_field, T.hash(String, Integer)
-      val :string_map_field, T.hash(String, String)
-      def initialize(int_map_field:, string_map_field:)
-        super method(__method__).parameters.map { |parts| [parts[1], eval(parts[1].to_s)] }.to_h
-      end
-      
-    end
-    
-    class OptionalFields
-      include DataClass
-      val :int_option_field, T.nilable(Integer)
-      val :string_option_field, T.nilable(String)
-      def initialize(int_option_field:, string_option_field:)
-        super method(__method__).parameters.map { |parts| [parts[1], eval(parts[1].to_s)] }.to_h
-      end
-      
-    end
-    
-    class RawJsonField
-      include DataClass
-      val :json_field, T.hash(String, Untyped)
-      def initialize(json_field:)
-        super method(__method__).parameters.map { |parts| [parts[1], eval(parts[1].to_s)] }.to_h
-      end
-      
-    end
-    
-    class OrderCreated
-      include DataClass
-      val :id, UUID
-      val :sku, String
-      val :quantity, Integer
-      def initialize(id:, sku:, quantity:)
-        super method(__method__).parameters.map { |parts| [parts[1], eval(parts[1].to_s)] }.to_h
-      end
-      
-    end
-    
-    class OrderChanged
-      include DataClass
-      val :id, UUID
-      val :quantity, Integer
-      def initialize(id:, quantity:)
-        super method(__method__).parameters.map { |parts| [parts[1], eval(parts[1].to_s)] }.to_h
-      end
-      
-    end
-    
-    class OrderCanceled
-      include DataClass
-      val :id, UUID
-      def initialize(id:)
-        super method(__method__).parameters.map { |parts| [parts[1], eval(parts[1].to_s)] }.to_h
-      end
-      
-    end
-    
-    OrderEvent = T.union(created: OrderCreated, changed: OrderChanged, canceled: OrderCanceled)
   end
+  
+  class Nested
+    include DataClass
+    val :field, String
+    def initialize(field:)
+      super method(__method__).parameters.map { |parts| [parts[1], eval(parts[1].to_s)] }.to_h
+    end
+    
+  end
+  
+  class Parent
+    include DataClass
+    val :field, String
+    val :nested, Nested
+    def initialize(field:, nested:)
+      super method(__method__).parameters.map { |parts| [parts[1], eval(parts[1].to_s)] }.to_h
+    end
+    
+  end
+  
+  class Choice
+    include Enum
+    define :first_choice, 'FIRST_CHOICE'
+    define :second_choice, 'SECOND_CHOICE'
+    define :third_choice, 'THIRD_CHOICE'
+  end
+  
+  class EnumFields
+    include DataClass
+    val :enum_field, Choice
+    def initialize(enum_field:)
+      super method(__method__).parameters.map { |parts| [parts[1], eval(parts[1].to_s)] }.to_h
+    end
+    
+  end
+  
+  class NumericFields
+    include DataClass
+    val :byte_field, Integer
+    val :short_field, Integer
+    val :int_field, Integer
+    val :long_field, Integer
+    val :float_field, Float
+    val :double_field, Float
+    val :decimal_field, Float
+    def initialize(byte_field:, short_field:, int_field:, long_field:, float_field:, double_field:, decimal_field:)
+      super method(__method__).parameters.map { |parts| [parts[1], eval(parts[1].to_s)] }.to_h
+    end
+    
+  end
+  
+  class NonNumericFields
+    include DataClass
+    val :boolean_field, Boolean
+    val :char_field, String
+    val :string_field, String
+    val :uuid_field, UUID
+    val :date_field, Date
+    val :datetime_field, DateTime
+    val :time_field, TimeOfDay
+    def initialize(boolean_field:, char_field:, string_field:, uuid_field:, date_field:, datetime_field:, time_field:)
+      super method(__method__).parameters.map { |parts| [parts[1], eval(parts[1].to_s)] }.to_h
+    end
+    
+  end
+  
+  class ArrayFields
+    include DataClass
+    val :int_array_field, T.array(Integer)
+    val :string_array_field, T.array(String)
+    def initialize(int_array_field:, string_array_field:)
+      super method(__method__).parameters.map { |parts| [parts[1], eval(parts[1].to_s)] }.to_h
+    end
+    
+  end
+  
+  class MapFields
+    include DataClass
+    val :int_map_field, T.hash(String, Integer)
+    val :string_map_field, T.hash(String, String)
+    def initialize(int_map_field:, string_map_field:)
+      super method(__method__).parameters.map { |parts| [parts[1], eval(parts[1].to_s)] }.to_h
+    end
+    
+  end
+  
+  class OptionalFields
+    include DataClass
+    val :int_option_field, T.nilable(Integer)
+    val :string_option_field, T.nilable(String)
+    def initialize(int_option_field:, string_option_field:)
+      super method(__method__).parameters.map { |parts| [parts[1], eval(parts[1].to_s)] }.to_h
+    end
+    
+  end
+  
+  class RawJsonField
+    include DataClass
+    val :json_field, T.hash(String, Untyped)
+    def initialize(json_field:)
+      super method(__method__).parameters.map { |parts| [parts[1], eval(parts[1].to_s)] }.to_h
+    end
+    
+  end
+  
+  class OrderCreated
+    include DataClass
+    val :id, UUID
+    val :sku, String
+    val :quantity, Integer
+    def initialize(id:, sku:, quantity:)
+      super method(__method__).parameters.map { |parts| [parts[1], eval(parts[1].to_s)] }.to_h
+    end
+    
+  end
+  
+  class OrderChanged
+    include DataClass
+    val :id, UUID
+    val :quantity, Integer
+    def initialize(id:, quantity:)
+      super method(__method__).parameters.map { |parts| [parts[1], eval(parts[1].to_s)] }.to_h
+    end
+    
+  end
+  
+  class OrderCanceled
+    include DataClass
+    val :id, UUID
+    def initialize(id:)
+      super method(__method__).parameters.map { |parts| [parts[1], eval(parts[1].to_s)] }.to_h
+    end
+    
+  end
+  
+  OrderEvent = T.union(created: OrderCreated, changed: OrderChanged, canceled: OrderCanceled)
 end
 
--- test/client_test.rb
+++ test/client_test.rb
@@ -4,36 +4,34 @@ require 'test_service_client/models'
 require 'test_service_client/client'
 
 module TestService
-  module Client
-    class ClientTests < Test::Unit::TestCase
-      def test_echo_body
-        request_body = Message.new(int_field: 123, string_field: "the string")
-        client = EchoClient.new(URI(ENV["SERVICE_URL"]))
-        response_body = client.echo_body(body: request_body)
-        assert_true T.instance_of?(Message, response_body)
-        assert_equal request_body, response_body
-      end
+  class ClientTests < Test::Unit::TestCase
+    def test_echo_body
+      request_body = Message.new(int_field: 123, string_field: "the string")
+      client = EchoClient.new(URI(ENV["SERVICE_URL"]))
+      response_body = client.echo_body(body: request_body)
+      assert_true T.instance_of?(Message, response_body)
+      assert_equal request_body, response_body
+    end
 
-      def test_echo_query_parameters
-        client = EchoClient.new(URI(ENV["SERVICE_URL"]))
-        response_body = client.echo_query(int_query: 123, string_query: "the string")
-        assert_true T.instance_of?(Message, response_body)
-        assert_equal Message.new(int_field: 123, string_field: "the string"), response_body
-      end
+    def test_echo_query_parameters
+      client = EchoClient.new(URI(ENV["SERVICE_URL"]))
+      response_body = client.echo_query(int_query: 123, string_query: "the string")
+      assert_true T.instance_of?(Message, response_body)
+      assert_equal Message.new(int_field: 123, string_field: "the string"), response_body
+    end
 
-      def test_echo_header
-        client = EchoClient.new(URI(ENV["SERVICE_URL"]))
-        response_body = client.echo_header(int_header: 123, string_header: "the string")
-        assert_true T.instance_of?(Message, response_body)
-        assert_equal Message.new(int_field: 123, string_field: "the string"), response_body
-      end
+    def test_echo_header
+      client = EchoClient.new(URI(ENV["SERVICE_URL"]))
+      response_body = client.echo_header(int_header: 123, string_header: "the string")
+      assert_true T.instance_of?(Message, response_body)
+      assert_equal Message.new(int_field: 123, string_field: "the string"), response_body
+    end
 
-      def test_echo_url_params
-        client = EchoClient.new(URI(ENV["SERVICE_URL"]))
-        response_body = client.echo_url_params(int_url: 123, string_url: "value")
-        assert_true T.instance_of?(Message, response_body)
-        assert_equal Message.new(int_field: 123, string_field: "value"), response_body
-      end
+    def test_echo_url_params
+      client = EchoClient.new(URI(ENV["SERVICE_URL"]))
+      response_body = client.echo_url_params(int_url: 123, string_url: "value")
+      assert_true T.instance_of?(Message, response_body)
+      assert_equal Message.new(int_field: 123, string_field: "value"), response_body
     end
   end
 end
